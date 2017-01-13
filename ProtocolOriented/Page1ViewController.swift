78b15e31183152034dc2dee0323267b5c66e7d10
Fatih YILDIZHAN <fatih@momencraft.com>
2017-01-13 18:47:59 +0300

    added new pages

---
+++ ProtocolOriented/Page1ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Page1ViewController.swift
+//  ProtocolOriented
+//
+//  Created by Fatih YILDIZHAN on 13/01/2017.
+//  Copyright © 2017 7renk. All rights reserved.
+//
+
+import UIKit
+
+class Page1ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+  
+}
---
+++ ProtocolOriented/Page2ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  Page2ViewController.swift
+//  ProtocolOriented
+//
+//  Created by Fatih YILDIZHAN on 13/01/2017.
+//  Copyright © 2017 7renk. All rights reserved.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+}
