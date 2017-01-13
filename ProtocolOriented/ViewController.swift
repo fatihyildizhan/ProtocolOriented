c2c470b72d5b95f7818901ce13a0c507ec43b0d1
Fatih YILDIZHAN <fatih@momencraft.com>
2017-01-13 17:50:07 +0300

    Initial Commit

---
+++ ProtocolOriented/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProtocolOriented
+//
+//  Created by Fatih YILDIZHAN on 13/01/2017.
+//  Copyright © 2017 7renk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
